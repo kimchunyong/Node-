0300cb87cf4ae62dd2f02dee6e22e17c779630ef
kimchunyong <lovemy5854@naver.com>
2019-04-27 14:39:21 +0900

    [edit] code

--- main.js
+++ main.js
@@ -2,21 +2,20 @@ const http = require('http');
 const fs = require('fs');
 
 const createServer = (server, port) => {
-    http.createServer(server).listen(port);
-}
+  http.createServer(server).listen(port);
+};
 
 const app = (request, response) => {
-    const url = request.url;
-    if (request.url == '/') {
-        url = '/index.html';
-    }
-    if (request.url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
-}
+  const url = request.url;
+  if (request.url == '/') {
+    url = '/index.html';
+  }
+  if (request.url == '/favicon.ico') {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  response.end(fs.readFileSync(__dirname + url));
+};
 
 createServer(app, 3000);
 createServer(app, 5000);
-
