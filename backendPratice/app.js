5d572db1d64681b764018d4b5529c0cdaac92915
kimchunyong <lovemy58511@gmail.com>
2019-11-24 21:19:20 +0900

    node with ajax 통신

--- backendPratice/app.js
+++ backendPratice/app.js
@@ -13,6 +13,9 @@ app.use(express.static('public'));
 app.use(bodyParser.json()); //json형태
 app.use(bodyParser.urlencoded({ extended: true })); //클라이언트에서 json형태로 넘어오지않을시
 
+//템플릿 엔진 설정
+app.set('view engine', 'ejs');
+
 //url routing
 app.get('/', function (req, res) {
     //res.send('<h1>hi frend!</h1>')
@@ -28,5 +31,12 @@ app.post('/email_post', function (req, res) {
     //get : req.param('email');
     //post 는 body parser로 파싱 필요
     console.log(req.body.email)
-    res.send('<h1>welcome !' + req.body.email + '</h1>');
+    //res.send('<h1>welcome !' + req.body.email + '</h1>');
+    res.render('email.ejs', { 'email': req.body.email });
+});
+
+app.post('/ajax_send_email', function (req, res) {
+    console.log(req.body.email);
+    var responseData = { 'result': 'ok', 'email': req.body.email };
+    res.json(responseData);
 });
